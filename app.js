5c42967ce51eefdab0a135fef4089d869ee00555
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-08-19 10:28:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // import functions and grab DOM elements
-import { getRandomThrow } from "./get-random-throw.js";
-const button = document.querySelector('#guess');
-const choice = document.querySelector('#choice');
+import { getRandomThrow, doesUserWin, getPercent } from "./get-random-throw.js";
+
+const playButton = document.querySelector('#guess');
+//const Userchoice = document.querySelector('#choice');
 const winLose = document.querySelector('#win-or-lose');
-const wins = document.querySelector('#wins');
-const losses = document.querySelector('#losses');
-const draw = document.querySelector('#draw');
-const total = document.querySelector('#total');
-const percent = document.querySelector('#percent');
+const winsEl = document.querySelector('#wins');
+const lossesEl = document.querySelector('#losses');
+const draws = document.querySelector('#draw');
+const totalEl = document.querySelector('#total');
+const percentage = document.querySelector('#percent');
+const resetEl = document.querySelector('#reset');
 
-
+let wins = 0; 
+let losses = 0; 
+let draw = 0; 
+let total = 0;
+let percent =0;  
 
 // initialize global state
 
@@ -17,3 +23,33 @@ const percent = document.querySelector('#percent');
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  playButton.addEventListener('click', () => {
+    // pulling the computer generated guess
+    const gamePlay = getRandomThrow();
+    total++;
+    // pulling the user selection upon click and turning into var.
+    const checkedRadioButton = document.querySelector(':checked');
+    const userChoice = checkedRadioButton.value;
+    
+    
+    if (userChoice === gamePlay) {
+        // eslint-disable-next-line no-const-assign
+        draws++;
+        draws.textContent = draws;
+    } else if (doesUserWin(userChoice, gamePlay) === true) {
+        wins++;
+        winsEl.textContent = wins;
+    } else if (doesUserWin(userChoice, gamePlay) === false) {
+        losses++;
+        lossesEl.textContent = losses;
+    }
+    
+    let winning = (wins / total) * 100;
+    percent = Math.round(winning);
+    percentage.textContent = `Success: ${percent}%`;
+
+    
+});
+resetEl.addEventListener('click',()=>{
+  window.location.reload();
+});
