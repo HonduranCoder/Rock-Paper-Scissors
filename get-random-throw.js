4bffb4f03cb58ce6ff07f4a0deb7aae47ee9105a
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-08-19 10:29:23 -0500

    Update get-random-throw.js

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,3 +1,45 @@
 export function getRandomThrow(){
-    
+    const randomChoice = Math.round(Math.random() *3);
+    //const choiceArray = ['rock', 'paper', 'scissors'];
+    //return choiceArray[randomChoice];
+    let compChoice; 
+
+    if (randomChoice === 1) {
+        compChoice = 'rock';
+    } else if (randomChoice === 2) {
+        compChoice = 'paper';
+    } else if (randomChoice === 3) {
+        compChoice = 'scissors';
+    }
+
+    return compChoice;
+}
+
+let draw = 0;
+
+
+export function doesUserWin(userChoice, compChoice){
+    if (userChoice === compChoice) {
+        return draw; 
+} else if (userChoice === 1 && compChoice === 2) {
+        return 'lose';
+// eslint-disable-next-line eqeqeq
+} else if (userChoice == 1 && compChoice == 3){
+        return 'win';
+} else if (userChoice === 2 && compChoice === 3){
+        return 'lose';
+} else if (userChoice === 2 && compChoice === 1){
+        return 'win'; 
+} else if (userChoice === 3 && compChoice === 2){
+        return 'win'; 
+} else if (userChoice === 3 && compChoice === 1){
+        return 'lose';
+}
+}
+
+export function getPercent(wins,total){
+    const percent = wins/total * 100; 
+    const roundedPercent = Math.round(percent);
+
+    return roundedPercent;
 }
